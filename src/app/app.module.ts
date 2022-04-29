bda8d3878cc4342171da7963c79adc02ff668aaf
Yazeed Idris <s201730270@kfupm.edu.sa>
2022-04-29 03:50:11 +0300

    - Custom styling added to stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {CookieService} from "ngx-cookie-service";
 import {FormsModule} from "@angular/forms";
+import { ProjectDisplayComponent } from './project-display/project-display.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectDisplayComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule,
