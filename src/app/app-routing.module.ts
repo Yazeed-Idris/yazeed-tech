bf2e3dd990e42fba4faf89a498183ea30d08719b
yazed <s201730270@kfupm.edu.sa>
2022-05-31 06:15:53 +0300

    - Navigation is much better now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import {ContactComponent} from "./contact/contact.component";
 import {ProjectsComponent} from "./projects/projects/projects.component";
 
 const routes: Routes = [
-  {path: '', component: AboutComponent},
   {path: 'javascript30', loadChildren: () => import('./javascript30/javascript30.module').then(m => m.Javascript30Module)},
   {path: 'about', component: AboutComponent},
   {path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)},
