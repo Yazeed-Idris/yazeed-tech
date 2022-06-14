0c9f9d20a445718a268f7c610bd605201035b950
yazed <s201730270@kfupm.edu.sa>
2022-06-14 23:47:31 +0300

    - Made an interactive function thingy

--- src/app/jsfunctions/filter-function/filter-function.component.ts
+++ src/app/jsfunctions/filter-function/filter-function.component.ts
@@ -1,25 +1,35 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-filter-function',
   templateUrl: './filter-function.component.html',
   styleUrls: ['./filter-function.component.scss']
 })
-export class FilterFunctionComponent implements OnInit, OnChanges {
+export class FilterFunctionComponent implements OnInit {
   result: any[];
   input: any[]
   inputFunction: any;
 
   constructor() {
     this.input = [1, 2, 3, 4]
-
+    this.result = this.input.filter((el) => el > 0);
+    this.inputFunction = eval('(el) => el > 0')
   }
 
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: any): void {
-    console.log('changes:', changes);
+  arrayChanged($event) {
+    this.input = $event.target.value.split(',');
+    this.filterArray();
   }
 
+  functionChanged($event) {
+    this.inputFunction = eval($event.target.value);
+    this.filterArray();
+  }
+
+  filterArray() {
+    this.result = this.input.filter(this.inputFunction);
+  }
 }
